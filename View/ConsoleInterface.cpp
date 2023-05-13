43d426164db7735ed8ba1e425873b93bf040687e
Annabel Giles <annabel.giles@studio.unibo.it>
2023-05-13 17:17:23 +0200

    Create console interface

--- View/ConsoleInterface.cpp
+++ View/ConsoleInterface.cpp
@@ -1,5 +1,42 @@
-namespace View {
-    class ConsoleInterface {
+#include <iostream>
 
-    };
-}
+#include "../Controller/Simulation.cpp"
+
+namespace View {
+class ConsoleInterface {
+ public:
+  ConsoleInterface() {}
+  void start() {
+    int S, I, R;
+    float beta, gamma;
+    std::cout << "Insert initial suscetibles: ";
+    std::cin >> S;
+    std::cout << "Insert initial infected: ";
+    std::cin >> I;
+    std::cout << "Insert initial removed: ";
+    std::cin >> R;
+    std::cout << "Insert parameter beta: ";
+    std::cin >> beta;
+    std::cout << "Insert parameter gamma: ";
+    std::cin >> gamma;
+    Controller::Simulation simulation{S, I, R, beta, gamma};
+    std::cout << "Simulation successfully created!\n";
+    int t;
+    std::cout << "Insert number of stages to calculate: ";
+    std::cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        simulation.advanceStage();
+        std::cout << simulation.getCurrentStateInfo() << ".\n";      
+    }
+
+    // prende input dall'utente per cambiare gli stage o chiudere il programma
+    // gestisce anche la creazione di grafici o cose così
+  }
+  void stop() {}
+
+  // DOPO
+  // fare un costruttore vuoto e uno con i dati in input
+  // (nel caso vengano passati da linea di comando tramite il main)
+};
+}  // namespace View
--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 // Per compilare
-// g++ -std=c++2a -Wall -Wextra -fsanitize=address,undefined main.cpp Model/*.cpp View/*.cpp Controller/*.cpp -o app
-// Per eseguire
+// g++ -std=c++2a -Wall -Wextra -fsanitize=address,undefined main.cpp
+// Model/*.cpp View/*.cpp Controller/*.cpp -o app Per eseguire
 // ./app
+#include "View/ConsoleInterface.cpp"
 
 int main(int argc, char const *argv[]) {
-    
-    return 0;
+  // new ConsoleInterface() e basta
+  View::ConsoleInterface consoleInterface{};
+  consoleInterface.start();
+
+  // se argc non è 0 quindi ci stanno degli argomenti passati a linea di comando
+  // quindi va create la interface con i dati passati dentro
+  return 0;
 }
