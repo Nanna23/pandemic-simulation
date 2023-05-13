ce751d69901bee8e1c562a7bbf73c08d6ad3442f
Annabel Giles <annabel.giles@studio.unibo.it>
2023-05-13 17:16:56 +0200

    Fix population not being updated

--- Model/Pandemic.cpp
+++ Model/Pandemic.cpp
@@ -1,10 +1,11 @@
+#include <iostream>
 #include "Pandemic.hpp"
-
 #include "Population.hpp"
 
 namespace Model {
 Model::Pandemic::Pandemic(double beta, double gamma)
     : beta{beta}, gamma{gamma} {}
+//per usare stesso population ma cambiare i valori dentro uso i puntatori
 void Model::Pandemic::calculateNextStage(Population* populationPointer) {
   Population population = *populationPointer;
   int S = population.getS();
@@ -14,6 +15,11 @@ void Model::Pandemic::calculateNextStage(Population* populationPointer) {
   int newS = S - beta * (S * I) / N;
   int newI = I + beta * (S * I) / N - gamma * I;
   int newR = R + gamma * I;
-  populationPointer = new Model::Population(newS, newI, newR);
+  //dall'indirizzo di casa ottengo la casa presente lì
+  //e la sostituisco con una nuova casa con i dati aggiornati
+  //sostitusco la population a quel indirizzo con una aggiornata prendendo
+  //da un altro indirizzo la population nuova
+  //new model::Population mi da il pointer
+  (*populationPointer) = *(new Model::Population(newS, newI, newR));
 }
 }  // namespace Model
