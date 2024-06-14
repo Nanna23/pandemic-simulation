4f8e2a2798349101c8f581738f9420497b9a6a16
Annabel Giles <annabel.giles@studio.unibo.it>
2024-06-14 13:31:37 +0200

    Finish simulation test

--- test/simulation.test.cpp
+++ test/simulation.test.cpp
@@ -6,9 +6,16 @@
 
 TEST_CASE("Testing the class handling a simulation") {
   controller::Simulation simulation{1000, 500, 50, 0.5, 0.7, 1};
-  SUBCASE("Testing the stage count of a simulation") {
+  SUBCASE("Testing creation of a simulation with negative t") {
+    CHECK_THROWS_AS(new controller::Simulation(1000, 500, 50, 0.5, 0.7, -4), const std::invalid_argument&);
+  }
+  SUBCASE("Testing advancing stages of a simulation") {
     CHECK(simulation.getStageCount() == 0);
     CHECK(simulation.advanceStage());
+    CHECK(simulation.getStageCount() == 1);
+
+    // checking that simulation does not advance past t stages
+    CHECK_FALSE(simulation.advanceStage());
     CHECK(simulation.getStageCount() == 1);
   }
 }
