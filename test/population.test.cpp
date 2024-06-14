3b7b22112c6f10437266d5540481c5b37cf22279
Annabel Giles <annabel.giles@studio.unibo.it>
2024-06-14 12:59:08 +0200

    Finish test on population

--- test/population.test.cpp
+++ test/population.test.cpp
@@ -10,8 +10,13 @@ TEST_CASE("Testing the class handling a population") {
   SUBCASE("Testing creation of a population with no people inside") {
     CHECK_THROWS_AS(new model::Population(0, 0, 0), const std::invalid_argument&);
   }
-  SUBCASE("Testing update of population with wrong data") {
+  SUBCASE("Testing creation of a population with negative values") {
+    CHECK_THROWS_AS(new model::Population(-4, -4, -4), const std::invalid_argument&);
+  }
+  SUBCASE("Testing update of population with more people then the beggining") {
     CHECK_FALSE(population.update(1000, 1000, 1000));
+
+    // Checking that the update what not done with the wrong values
     CHECK(population.getS() == 500);
     CHECK(population.getI() == 100);
     CHECK(population.getR() == 50);
@@ -24,4 +29,13 @@ TEST_CASE("Testing the class handling a population") {
     CHECK(population.getR() == 50);
     CHECK(population.getN() == 650);
   }
+  SUBCASE("Testing update of population with negative values") {
+    CHECK_FALSE(population.update(-400, -200, -50));
+    
+    // Checking that the update what not done with the wrong values
+    CHECK(population.getS() == 500);
+    CHECK(population.getI() == 100);
+    CHECK(population.getR() == 50);
+    CHECK(population.getN() == 650);
+}
 }
